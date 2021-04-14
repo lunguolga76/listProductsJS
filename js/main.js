6bdc944b430a8fcd0fb6e178e863dc9c18cb0c11
Lungu Olga <lunguolga2000@yahoo.com>
2021-04-14 11:41:32 +0300

    the 4th commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let div = document.getElementById('cardBody');
 let cartProducts=[];
 let printCartList=document.getElementById('printCartList')
 let clickCountProduct=0;
-let totalSum=0;
+let grandTotal=0;
 let printTotalPrice=document.getElementById('printTotalPrice')
 
 
@@ -15,7 +15,8 @@ let newProduct = {
    productDescription: document.getElementById('productDescription').value,
    productPrice:Number(document.getElementById("productPrice").value),
    productAvailability:document.getElementById("productAvailability").checked==true,
-   productAvailability: productAvailability.checked==true  ? '<small>In stock</small>' : '<small>Out of stock<small>',
+   productAvailability: productAvailability.checked==true  ? 'In stock' : 'Out of stock',
+
   
 }
    products.push(newProduct);
@@ -25,58 +26,71 @@ let newProduct = {
 }
 const modifyHTML = () => {
 div.innerHTML="";
-
-products.forEach(function(product,i) {
-   
+Object.entries(products).forEach(([i,product])=> {
+   //console.log(products);
 div.innerHTML +=  `
 <div class="bg-image card shadow-1-strong rounded-2">
 <div class="card-body text-white">
   <h4 class="card-title">${product.productName}</h4>
   <p class="card-text"> ${product.productDescription}</p>
-<h5>Product price:${product.productPrice}$</h5> 
+<h5>Product price:${product.productPrice}$</h5>
 </div>
 <div class="d-flex  d-flex justify-content-center align-items-center">
   <button type="button" id="productAvailable"class="btn btn-primary m-1">${product.productAvailability}</button>
   <button type="button" id="notInStock"class="btn btn-danger m-2"><a onclick="deleteProduct()"><i class="fa fa-trash"></i></a></button>
-  <button id="addToCart" type="button" disabled class="btn btn-success"><a onclick="addToCart()"><small>Add to cart</small></a></button>
+  <button id="addToCart" type="button" class="btn btn-success"><a onclick="addToCart()"><small>Add to cart</small></a></button>
 </div>
 </div>
 <br />
 `
-if(product.productAvailability=="in stock"){
-   document.getElementById('addToCart').disabled=true;
+/*if(product.productAvailability=="in stock"){
+   //console.log(product.productAvailability.checked),
+   div.innerHTML +=document.getElementById('addToCart').disabled=false;
+   
 }else{
-   document.getElementById('addToCart').disabled=false;
+   div.innerHTML +=document.getElementById('addToCart').disabled=true;
 
-}
+}*/
 
 });
 };
+
+/*const EnableDisable=()=> {
+   div.innerHTML="";
+   alert("ok");
+   //Reference the Button.
+   products.forEach(function(product,i) {
+      console.log(products)
+   //Verify the TextBox value.
+   if (product.productAvailability.checked==true) {
+      document.getElementById("addToCart").disabled =false;
+   } else {
+       //Disable the TextBox when TextBox is empty.
+       document.getElementById("addToCart").disabled =true;
+   }
+});
+}*/
 const deleteProduct=(j) =>  {
 products.splice(j,1);
 modifyHTML();
 };
 
 const addToCart=()=>{ 
-  
         printCartList.innerHTML="";
+        printTotalPrice.innerHTML="";
         let grandTotal=0;
+        clickCountProduct= clickCountProduct+1;
+        console.log(clickCountProduct);
+       document.getElementById("addToCart").addEventListener("onclick",addToCart);
          cartProducts.push(Object.entries(products).forEach(([i,v])=>{ 
           printCartList.innerHTML+=`<h6>Product name:${v.productName}<h2>
                                     <h6>Product price:${v.productPrice}$*${clickCountProduct}<h2>`
-                                 
-         
+                                     
+                                       grandTotal+=v.productPrice*clickCountProduct;
+         printTotalPrice.innerHTML=`<h4>TOTAL PRICE:${grandTotal}$</h4>`
 }
    ))
    
 }
-
-/*function totalSum(){
-let totalSum=0;
-printTotalPrice.innerHTML="";
-Object.values(products).forEach(i=>{
-totalSum+=perseInt(i.productsPrice[i].printTotalPrice)
-})
-}*/
 
 document.getElementById("myButton").addEventListener("click",addProduct);
